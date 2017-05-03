7763fd39d1aca0e3e332811035917465cbbde1f3
Filip Klembara <leo@navel.pro>
2017-05-03 18:38:08 +0200

    doc

--- src/workingpackview.h
+++ src/workingpackview.h
@@ -18,16 +18,74 @@ class WorkingPackView : public QFrame, public CardStacks::WorkingPack
     Q_OBJECT
 
 public:
+    /**
+     * construct view represention of working pack
+     *
+     * @param parent parenting view
+     * @return new object
+     */
     explicit WorkingPackView(QWidget *parent = 0);
+
+    /**
+     * push invisible card
+     *
+     * @param card card
+     */
     void push_invisible(card::Card card);
+
+    /**
+     * turn invisible card
+     */
     void turn_invisible();
+
+    /**
+     * event filter
+     *
+     * @param obj sender
+     * @param e event
+     * @return if we are interested in current evet
+     */
     bool eventFilter(QObject *obj, QEvent *e);
+
+    /**
+     * painting
+     *
+     * @param e event
+     */
     void paintEvent(QPaintEvent *e);
+
+    /**
+     * pop card
+     */
     void pop();
+
+    /**
+     * push card
+     *
+     * @param card card
+     */
     void push(card::Card card);
     CardSelection * selectionDelegate;
+
+    /**
+     * set cards
+     *
+     * @param stack data
+     */
     void setCards(CardStacks::GenericCardStack stack);
+
+    /**
+     * checking for move completion
+     *
+     * @param e event
+     */
     void mousePressEvent(QMouseEvent *e);
+
+    /**
+     * highlight cards
+     *
+     * @param count number of cards
+     */
     void highlight(unsigned int count);
     ~WorkingPackView();
 
