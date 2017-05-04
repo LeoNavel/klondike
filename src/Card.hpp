e59051a1f42e385a329acec9c6d0f41da3a58a1c
Adrian Tomasov <atomasov@redhat.com>
2017-05-04 21:12:44 +0200

    more comments

--- src/Card.hpp
+++ src/Card.hpp
@@ -12,15 +12,21 @@
 
 #include <iostream>
 
+/**
+ * @brief Card namespace
+ *
+ * This namespace shield definitions of class Card and constants important
+ * to this object.
+ */
 namespace card {
 
 
-    const int KING = 13;
-    const int QUEEN = 12;
-    const int JACK = 11;
-    const int ACE = 1;
+    const int KING = 13; /**< Const for KING*/
+    const int QUEEN = 12; /**< Const for QUEEN*/
+    const int JACK = 11; /**< Const for JACK*/
+    const int ACE = 1; /**< Const for ACE*/
 
-    const int MAX_CARD_IN_DECK = 13;
+    const int MAX_CARD_IN_DECK = 13; /**< Number of cards in card deck.*/
 
     enum sign {
         SPADES, HEART, CLUBS, DIAMONDS
@@ -35,18 +41,86 @@ namespace card {
         int number;
         bool visible;
     public:
+
+        /**
+         * Check if cards are same.
+         * @param other Seccond card object
+         * @return
+         */
         bool operator ==(Card other);
+
+        /**
+         * Implementing non equal check of cards.
+         * @param other Second card object.
+         * @return
+         */
         bool operator !=(Card other);
+
+        /**
+         * Card constructor
+         * @param number Card number
+         * @param card_sign Card sign
+         */
         Card(int number = 0, sign card_sign = SPADES);
+
+        /**
+         * Card constructor
+         * @param number Card number
+         * @param card_sign  Card sign
+         * @param visible visibility
+         */
         Card(int number, sign card_sign, bool visible);
+
+        /**
+         * Set card number.
+         * @param number
+         */
         void set_number(int number);
+
+        /**
+         * Set card sign.
+         * @param card_sign
+         */
         void set_sign(sign card_sign);
+
+        /**
+         * Get number of card.
+         * @return
+         */
         int get_number() const;
+
+        /**
+         * Return card sign.
+         * @return
+         */
         sign get_sign() const;
+
+        /**
+         * Return visibility attribute.
+         * @return
+         */
         bool get_visibility();
+
+        /**
+         * Get color of card.
+         * @return
+         */
         color get_color();
+
+        /**
+         * Check if card is visible and return result.
+         * @return
+         */
         bool isTurnedUp();
+
+        /**
+         * Making card visible.
+         */
         void turnUp();
+
+        /**
+         * Making card invisible.
+         */
         void turnDown();
 
         std::string print_card();
